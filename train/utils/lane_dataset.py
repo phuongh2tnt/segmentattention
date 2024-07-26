809d74958cb846c3294a9e62364e442af9149d1a
lethimyle60cntt3 <le.ltm.60cntt@ntu.edu.vn>
2024-07-26 11:23:33 +0700

    day du lieu len

---
+++ train/utils/lane_dataset.py
@@ -0,0 +1,68 @@
+"""
+UOW, 14/07/2022
+"""
+import collections
+from PIL import Image
+from torch.utils.data import Dataset
+import torchvision.transforms as T
+import numpy as np
+import cv2
+import torch
+import random
+import math
+import os
+import glob
+from operator import itemgetter
+from shutil import copyfile
+import warnings
+warnings.filterwarnings('ignore')
+
+
+class LaneDataset(Dataset):
+    def __init__(self, dataset_dir='./dataset/', subset='test', img_size=480):
+        """
+        :param dataset_dir: directory containing the dataset
+        :param subset: subset that we are working on ('train'/'test'/'valid')
+        :param img_size: image size
+        """
+        super(LaneDataset, self).__init__()
+        self.filenames = collections.defaultdict(list)
+        self.img_size = img_size
+        self.resize_img = T.Resize((img_size, img_size), interpolation=T.InterpolationMode.BILINEAR)
+        self.resize_gt = T.Resize((img_size, img_size), interpolation=T.InterpolationMode.NEAREST)
+        self.subset = subset
+        self.data_path = dataset_dir + '/' + subset
+        text_file = "{}/{}/{}.txt".format(dataset_dir, subset, subset)
+
+        # Read the text file
+        with open(text_file, 'r') as f:
+            self.filenames = f.read().splitlines()
+        print('Loaded {} subset with {} images'.format(subset, self.__len__()))
+
+    def __len__(self):
+        """
+        :return: length of the dataset (i.e., number of images)
+        """
+        return len(self.filenames)
+
+    def __getitem__(self, index):
+        filename = self.filenames[index]
+
+        # Read an image and its ground-truth
+        img = Image.open(self.data_path + '/images/' + 'DSC_' + filename + '.jpg').convert('RGB')    
+        #bài của thầy thanh
+        #gt = Image.open(self.data_path + '/groundtruth/' + filename + '_lane.png')
+        
+        #gt = Image.open(self.data_path + '/groundtruth/' + filename + '_lane.png')
+        #bai tom
+        gt = Image.open(self.data_path + '/groundtruth/' 'DSC_' + filename + '.png')
+        # Resize the image and ground-truth
+        img = self.resize_img(img)
+        gt = self.resize_gt(gt)
+
+        # Convert both to Pytorch tensors
+        img = T.ToTensor()(img)  # [0, 1] normalization included
+        gt = torch.from_numpy(np.asarray(gt))  # must use 'torch.from_numpy' otherwise all pixels become 0
+
+        return img, gt
+
