b57db1fa6d3f90f1edf4e5dba44368d1cd746932
phuongh2tnt <166468012+phuongh2tnt@users.noreply.github.com>
2024-07-27 22:54:34 +0700

    Update unetrescbam.py

--- train/unetrescbam.py
+++ train/unetrescbam.py
@@ -26,13 +26,14 @@ class UNet(nn.Module):
         super(UNet, self).__init__()
         self.encoder = encoder
 
-        # Adjust the UpConv layers to match the encoder output channels
+        # Define UpConv layers with the correct channel sizes
         self.up1 = UpConv(2048, 1024)  # from layer4
-        self.up2 = UpConv(1024 + 1024, 512)   # from layer3 + up1
-        self.up3 = UpConv(512 + 512, 256)    # from layer2 + up2
-        self.up4 = UpConv(256 + 256, 64)     # from layer1 + up3
+        self.up2 = UpConv(1024 + 1024, 512)  # from layer3 + up1
+        self.up3 = UpConv(512 + 512, 256)  # from layer2 + up2
+        self.up4 = UpConv(256 + 256, 64)  # from layer1 + up3
 
-        self.out_conv = nn.Conv2d(64 + 64, n_classes, kernel_size=1)  # 64 + 64 because of concatenation
+        # The final convolution layer should take 64 channels
+        self.out_conv = nn.Conv2d(64, n_classes, kernel_size=1)
 
     def forward(self, x):
         x1, x2, x3, x4 = self.encoder(x)
@@ -42,9 +43,8 @@ class UNet(nn.Module):
         x = self.up3(x, x1)   # layer2 to layer1
         x = self.up4(x, x)    # layer1 to output
 
-        x = self.out_conv(x)
+        x = self.out_conv(x)  # Final output
         return x
-
 def resnet50_cbam_unet(pretrained=True, n_classes=2):
     encoder = resnet50_cbam(pretrained=pretrained)
     model = UNet(encoder, n_classes)
